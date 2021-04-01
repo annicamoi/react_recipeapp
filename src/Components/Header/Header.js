7c68c7c9693ccd52d235cdbb84677cb766a59224
Moilanen Annika <s2100141@edu.bc.fi>
2021-04-01 16:21:03 +0300

    first commit for recipe base app

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+    return ( 
+        <header>
+        <div>
+        <h1> LOGO </h1> 
+        <Nav />
+        </div> 
+        </header>
+    );
+};
+
+export default Header;
+
---
+++ src/Components/Main/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>This is to share the delish recipes from my travels!</h1>
+        </div>
+    );
+};
+
+export default About;
